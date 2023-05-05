8157ad8daa61c1b826d390e43d3de79f49bdb1dd
kingtvs12 <122884145+kingtvs12@users.noreply.github.com>
2023-05-05 08:50:30 +0300

    Add files via upload

---
+++ NewsPaper/new/views.py
@@ -0,0 +1,88 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .filters import PostFilter
+from .forms import PostForm
+
+
+
+class PostsList(ListView):
+    model = Post
+    ordering = '-time_in'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['time_now'] = datetime.utcnow()
+        # context['text'] = None
+        return context
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['time_now'] = datetime.utcnow()
+        # context['text'] = None
+        return context
+
+
+class PostSearch(ListView):
+    model = Post
+    template_name = 'post_search.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
+
+
+class PostCreate(PermissionRequiredMixin, CreateView):
+    permission_required = 'new.add_post'
+    form_class = PostForm
+    model = Post
+    template_name = 'post_create.html'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        if self.request.method == 'POST':
+            path_info = self.request.META['PATH_INFO']
+            if path_info == '/new/create/':
+                post.view = 'NW'
+            elif path_info == '/articles/create/':
+                post.view = 'AR'
+        post.save()
+        return super().form_valid(form)
+
+
+class PostUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = 'new.change_post'
+    form_class = PostForm
+    model = Post
+    template_name = 'post_edit.html'
+
+
+class PostDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'new.delete_post'
+    model = Post
+    template_name = 'post_delete.html'
+
+
+
+
